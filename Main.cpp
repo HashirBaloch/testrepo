a47d26f4dd000d762de0ad976a6db5f6628da63f
HashirBaloch <138850705+HashirBaloch@users.noreply.github.com>
2023-07-07 11:10:06 +0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : CPPReuse.cpp
+// Author      : Dave
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "UsedCar.h"
+using namespace std;
+
+int main() {
+	UsedCar usedCar;
+	cout << usedCar.toString() << endl;
+	return 0;
+}
